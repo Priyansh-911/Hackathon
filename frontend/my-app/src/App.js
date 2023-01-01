b290f79860e78c59baa9a421d0f17aaad1c6ff45
Naman-003 <120316492+Naman-003@users.noreply.github.com>
2023-01-01 16:10:05 +0530

    changes done

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,9 +1,10 @@
-import { Button } from '@mui/material';
 import { ThemeProvider, Container } from '@mui/system';
 import { useEffect } from 'react';
 import Appbar from './component/appbar';
 import SignInOutContainer from './component/login/container';
 import theme from './styles/theme';
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
 
@@ -11,17 +12,28 @@ function App() {
     document.title = "React Material UI - Home";
   }, []);
   return (
-    <ThemeProvider theme={theme}>
-       <Container
-     maxWidth="xl"
-     sx={{
-       background: '#fff'
-     }} 
-     >
-      <Appbar/>
-       <Button variant='contained'>Test</Button>
-     </Container> 
-    </ThemeProvider>
+    <Router>
+    
+      <ThemeProvider theme={theme}>
+        <Container
+          maxWidth="xl"
+          sx={{
+            background: '#fff'
+          }}
+        >
+          <Appbar />
+          
+        </Container>
+      </ThemeProvider>
+        
+          
+          <Routes>
+            <Route path="/login" element={<SignInOutContainer/>}/>
+          </Routes>
+        
+
+     
+    </Router>
        
   );
   
