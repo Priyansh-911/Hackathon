78b3cc81b19d7b864454a9820b1d20c6f41a5432
Priyansh-911 <priyansh91103@gmail.com>
2023-01-02 19:45:14 +0530

    bug fix

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,26 @@
 const express = require("express");
 require('./db/config');
-const users = require("./db/users");
+const Users = require("./db/users");
 const scholarships  = require("./db/scholarships");
 const app = express();
 app.use(express.json());
 
 app.post("/register", async (req,res)=>{
-    res.send("progress")
-    let user = new user(req.body);
-    let result = await users.save();
+    res.send("progress");
+    let user = new Users(req.body);
+    let result = user.save();
     res.send(result);
-})
+});
 
-app.get("/scholarships", (req, res) => {
-    Products.find((err, data) => {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.status(200).send(data);
-      }
-    });
-  });
+// app.get("/scholarships", (req, res) => {
+//     Products.find((err, data) => {
+//       if (err) {
+//         res.status(500).send(err);
+//       } else {
+//         res.status(200).send(data);
+//       }
+//     });
+//   });
 
 app.listen(3000);
 
--- backend/db/users.js
+++ backend/db/users.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 
 // this is a sample schema
 // Note to lakshay == isko apni table ke hisaab se edit krlio theseee things should match the ones in table
-var Schema= mongoose.Schema();
+var Schema= mongoose.Schema;
 const userSchema = new Schema({
     name : String,
     email : String,
     gender : String,
-    DOB : Date,
+    DOB : String,
     state : String,
     std10 : Number,
     std12 : Number,
