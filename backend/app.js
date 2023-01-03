3efcdafb553413498ca62d410dccd9ce4ab9fb58
Priyansh-911 <priyansh91103@gmail.com>
2023-01-03 20:39:54 +0530

    bug fix

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,28 @@
 const express = require("express");
+const cors = require("cors");
 require('./db/config');
 const Users = require("./db/users");
 const scholarships  = require("./db/scholarships");
 const app = express();
+
 app.use(express.json());
+app.use(cors());
+
+app.get("/" , (req,res)=>{
+    res.status(200).send("up and running...");
+});
+
+
+app.get("/register", async (req,res)=>{
+    res.status(200).send("this is get...");
+});
+
+
 
 app.post("/register", async (req,res)=>{
-    res.send("progress");
+    res.status(200).send("progress");
     let user = new Users(req.body);
     let result = user.save();
-    res.send(result);
 });
 
 // app.get("/scholarships", (req, res) => {
